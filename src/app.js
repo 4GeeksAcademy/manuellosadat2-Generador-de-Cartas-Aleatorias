d97a2bbde74b2fc28dafcfc0d6e4bf138badcc10
manuellosadat2-a11y <manuellosadat2@gmail.com>
2026-01-05 03:49:20 +0000

    Generador de Cartas

--- src/app.js
+++ src/app.js
@@ -1,10 +1,3 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   let num = ["A","1","2","3","4","5","6","7","8","9","10","J", "Q", "K"];
     let cards = ["Hearts","Diamonds","Clubs","Spades"];
