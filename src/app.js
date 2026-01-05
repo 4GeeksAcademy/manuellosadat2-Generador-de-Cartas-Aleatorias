4f837844e93b2433e305f5833df5b588ff9ce927
manuellosadat2-a11y <manuellosadat2@gmail.com>
2026-01-05 03:35:10 +0000

    Generador de Cartas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let num = ["A","1","2","3","4","5","6","7","8","9","10","J", "Q", "K"];
+    let cards = ["Hearts","Diamonds","Clubs","Spades"];
+
+
+    let randomNum = Math.floor(Math.random() * num.length)
+    let randomCards = Math.floor(Math.random() * cards.length)
+
+    let cardsResult = cards[randomCards];
+
+    document.getElementById(`content`).innerHTML= num[randomNum]
+    document.getElementById('theCard').className = "";
+    document.getElementById(`theCard`).classList.add("card");
+    document.getElementById(`theCard`).classList.add(getCardsClass(cardsResult))
+
+    function getCardsClass (cards){
+        switch(cards){
+            case "Hearts": return "cards-hearts";
+            case "Diamonds": return "cards-diamonds";
+            case "Clubs": return "cards-clubs";
+            case "Spades": return "cards-spades";
+        }
+    }
 };
